ace11a1f37d56a9c6799c602dbed867490c69fe6
Michael Lang <lang@zib.de>
2014-04-15 06:45:59 +0000

    OPUSVIER-2076 - MigrationTestCase::assertOutputContainsString() wurde verbessert
    git-svn-id: https://svn.zib.de/opus4dev/migration/trunk@13091 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/MigrationTestCase.php
+++ tests/MigrationTestCase.php
@@ -74,7 +74,7 @@ class MigrationTestCase extends PHPUnit_Framework_TestCase {
                 $outputContainsString = true;
             }
         }
-        $this->assertTrue($outputContainsString, self::$output);
+        $this->assertTrue($outputContainsString);
     }
 
     protected function assertOutputNotContainsString($string) {
